dd72d73e20f7be2fde2196b0038819c8b84cf97e
lee <lsh0814k@gmail.com>
2024-02-24 00:12:06 +0900

    Thread join

---
+++ src/main/java/com/concurrency/join/BasicJoinExample.java
@@ -0,0 +1,21 @@
+package com.concurrency.join;
+
+public class BasicJoinExample {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            try {
+                System.out.println("스레드가 3초 동안 작동합니다.");
+                Thread.sleep(3000);
+                System.out.println("스레드 작동 완료");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        thread.start();
+        System.out.println("메인 스레드가 다른 스레드의 완료를 기다립니다.");
+
+        thread.join();
+        System.out.println("메인 스레드가 계속 진행합니다.");
+    }
+}
