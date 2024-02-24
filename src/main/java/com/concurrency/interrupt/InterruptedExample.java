2b54d19bae0e92db6d99a76da72d11f829efe535
lee <lsh0814k@gmail.com>
2024-02-24 13:56:11 +0900

    Thread interrupt

---
+++ src/main/java/com/concurrency/interrupt/InterruptedExample.java
@@ -0,0 +1,24 @@
+package com.concurrency.interrupt;
+
+public class InterruptedExample {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            while(!Thread.interrupted()) {
+                System.out.println("스레드는 작동 중입니다.");
+            }
+
+            System.out.println("스레드가 인터럽트 되었습니다.");
+            System.out.println("인터럽트 상태 : " + Thread.currentThread().isInterrupted());
+        });
+
+        thread.start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        thread.interrupt();
+    }
+}
