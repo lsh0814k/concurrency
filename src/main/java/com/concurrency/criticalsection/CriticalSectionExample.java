9175c8ee8bc215b4176ca7c6611137c7f5d8b65f
lee <lsh0814k@gmail.com>
2024-02-25 17:39:16 +0900

    critical section & race condition

---
+++ src/main/java/com/concurrency/criticalsection/CriticalSectionExample.java
@@ -0,0 +1,33 @@
+package com.concurrency.criticalsection;
+
+public class CriticalSectionExample {
+    public static void main(String[] args) {
+        SharedResource resource = new SharedResource();
+
+        Thread t1 = new Thread(resource::increment);
+        Thread t2 = new Thread(resource::increment);
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class SharedResource {
+    private int counter = 0;
+
+    public void increment() {
+        for (int i = 0; i < 10000; i++) {
+            synchronized (this) { // Entry Section
+                counter++;
+                System.out.println(Thread.currentThread().getName() + ": " + counter);
+            } // Exit Section
+        }
+
+        // Remainder Section
+        doOtherWork();
+    }
+
+    private void doOtherWork() {
+        System.out.println(Thread.currentThread().getName() + " 는 Critical Section 외부에서 작업을 수행하고 있다.");
+    }
+}
