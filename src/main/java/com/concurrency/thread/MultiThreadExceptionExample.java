07cdf0d4bbd3102b0a68194d19a8c49b4fb571ed
lee <lsh0814k@gmail.com>
2024-02-25 15:56:27 +0900

    single Thread & multi Thread

---
+++ src/main/java/com/concurrency/thread/MultiThreadExceptionExample.java
@@ -0,0 +1,49 @@
+package com.concurrency.thread;
+
+public class MultiThreadExceptionExample {
+    private static Object lock = new Object();
+    private static int sum = 0;
+    public static void main(String[] args) throws InterruptedException {
+
+        long start = System.currentTimeMillis();
+
+        Thread thread = new Thread(() -> {
+            for (int i = 1; i <= 500; i++) {
+                synchronized (lock) {
+                    sum += i;
+                }
+
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 501; i <= 1000; i++) {
+                synchronized (lock) {
+                    sum += i;
+                }
+
+                try {
+                    Thread.sleep(1);
+                    throw new RuntimeException("error");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        thread.start();
+        thread2.start();
+
+        thread.join();
+        thread2.join();
+
+        System.out.println("합계 : " + sum);
+        System.out.println("멀티 스레드 처리 시간 : " + (System.currentTimeMillis() - start) + "ms");
+    }
+}
