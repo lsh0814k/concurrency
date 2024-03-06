9e734ec6406a16e5a6810a1f028dff55f703e2f6
lee <lsh0814k@gmail.com>
2024-03-06 23:14:03 +0900

    CompletableFuture.handle

---
+++ src/main/java/com/concurrency/completablefuture/HandleExample.java
@@ -0,0 +1,54 @@
+package com.concurrency.completablefuture;
+
+import java.util.concurrent.CompletableFuture;
+
+public class HandleExample {
+    public static void main(String[] args) {
+        CompletableFuture<Integer> cf1 = CompletableFuture.supplyAsync(() -> {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+
+                    return 10;
+                })
+                .handle((r, e) -> {
+                    if (e != null) {
+                        System.out.println("비동기 처리 예외 1: " + e.getMessage());
+                        return -1; // 예외 발생 시 기본 값 반환
+                    }
+                    return r;
+                });
+
+        CompletableFuture<Integer> cf2 = CompletableFuture.supplyAsync(() -> {
+                    try {
+                        Thread.sleep(500);
+                        throw new RuntimeException("error");
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+
+                    return 20;
+                })
+                .handle((r, e) -> {
+                    if (e != null) {
+                        System.out.println("비동기 예외 처리 2: " + e.getMessage());
+                        return -1;
+                    }
+
+                    return r;
+                });
+
+        CompletableFuture<Integer> cf3 = cf1.thenCombine(cf2, (r1, r2) -> {
+            if (r1 == -1 || r2 == -1) {
+                // 둘 중 하나라도 예외가 발생하면 예외 처리
+                return -1;
+            }
+            // 두 결과를 조합하여 복잡한 작업 수행
+            return r1 + r2;
+        });
+
+        System.out.println("final result: " + cf3.join());
+    }
+}
